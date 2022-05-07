fa2e26714e8d1a27cf87bf42f4cc04a4ed212a9e
Armin Wessel <armin.wessel95@gmail.com>
2022-05-07 11:15:00 +0200

    Add publisher for control error Change target to mirrored, also reasonable size
    in calc_error and calc_velocities: Add checks to verify that there are points defining current target and feature
    
    Add function get_Z, expand later into observer for distance
    
    rename function calc_interaction_matrix to update_interaction_matrix
     - now interaction matrix is returned but stored as local
    
    Return zero velocities if any input data is missing!

--- src/ibvservo.py
+++ src/ibvservo.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
 Image Based Visual Servoing
 """
 
+from attr import has
 import rospy
 import numpy as np
 from geometry_msgs.msg import Pose, Pose2D, Twist
@@ -20,8 +21,10 @@ class IBVSController():
         """
         self._pub = rospy.Publisher("cmd_vel", Twist, queue_size=1) # create publisher
         self._pub2 = rospy.Publisher("ibvs_target", Point2DArray, queue_size=1) # create publisher
+        self._pub3 = rospy.Publisher("control_error", Point2DArray, queue_size=1) # create publisher
         self._sub = rospy.Subscriber("ibvs_features", Point2DArray, self.feature_update_received) # create subscriber
-        self.set_target(utils.conv_nparray_2_Point2DArray(np.array([[200,200],[350,200],[350,350],[200,350]])))
+        center_target = utils.tf_cc(np.array([[40,40],[-40,40],[-40,-40],[40,-40]]))
+        self.set_target(utils.conv_nparray_2_Point2DArray(center_target))
 
     def set_target(self, target_coordinates : Point2DArray) -> None:
         if target_coordinates == None:
@@ -30,9 +33,15 @@ class IBVSController():
 
     
     def calc_error(self) -> np.array:
+        if (not hasattr(self.current_features,'points')) or (not hasattr(self.target, 'points')):
+            return np.zeros((4,2)) # return zero error if target or features are not defined
+        if len(self.current_features.points) == 0:
+            return np.zeros((4,2)) # return zero error if target or features are not defined
         _array_current = utils.conv_Point2DArray_2_nparray(self.current_features)
         _array_target = utils.conv_Point2DArray_2_nparray(self.target)
-        return _array_target-_array_current
+        error = _array_target-_array_current
+        self._pub3.publish(utils.conv_nparray_2_Point2DArray(error))
+        return error
 
 
     def set_target_depth(self, target_depth : float) -> None:
@@ -65,27 +74,31 @@ class IBVSController():
         """
         self.current_features = feature_coordinates # save current feature data 
         self._pub.publish(self.calc_velocities())   # calculate new velocities and publish the result 
-        self._pub2.publish(self.target)
+        self._pub2.publish(self.target) # publish the current target
         
 
-
-    def calc_interaction_matrix(self) -> np.ndarray:
+    def get_Z(self) -> float:
+        if hasattr(self, 'target_depth'):
+            return self.target_depth           # assume Z to be target depth
+        else:
+            rospy.logwarn('Assumed 0.1 for feature distance Z')
+            return 0.1 
+        
+    def update_interaction_matrix(self) -> None:
         """
         Calculate the interaction matrix L given the positions of 
         TODO include option to allow choosing between approximations of distance Z
         """
 
-        # check if there are at least 3 features
-        if self.current_features.length < 3:
-            raise ValueError('Feature vector is too short to calculate interaction matrix.')
+        if hasattr(self.current_features, 'points'):
+            if len(self.current_features.points)>0:
+                L = np.zeros([0,6])               # initialize empty matrix with 6 cols
+                Z = self.get_Z()
+                for point in self.current_features.points: 
+                    x, y = point.x, point.y
+                    L = np.append(L,[[-1/Z, 0, x/Z, x*y, -(1+x*x), y], [0, -1/Z, y/Z, 1+y*y, -x*y, -x]],axis=0)
+                self.L = L
 
-        L = np.zeros([0,6])               # initialize empty matrix with 6 cols
-        Z = self.target_depth           # assume Z to be target depth
-
-        for point in self.current_features.points: 
-            x, y = point.x, point.y
-            L = np.append(L,[[-1/Z, 0, x/Z, x*y, -(1+x*x), y], [0, -1/Z, y/Z, 1+y*y, -x*y, -x]],axis=0)
-        return L
 
     def calc_velocities(self, lambda_c = 1) -> Twist:
         """
@@ -93,13 +106,17 @@ class IBVSController():
         """
         velocity_cmd = Twist()                      # initialize empty Twist message
 
-        _L = self.calc_interaction_matrix()         
-        _L_inv = np.linalg.pinv(_L)
+        self.update_interaction_matrix()
 
-        _e = self.calc_error()     # calc error [[dx1, dy1],  [dx2, dy2],  [dx3, dy3]]
-        _e_flat = _e.flatten()                           # flaten error [dx1, dy1, dx2, dy2, dx3, dy3]
+        if not hasattr(self, 'L'): # If no interaction matrix is defined, set command velocity to all zeros
+            _vel=np.zeros(6)
+        else:
+            _L_inv = np.linalg.pinv(self.L)
 
-        _vel = - lambda_c * np.dot(_L_inv, _e_flat) # calculate velocity command vector
+            _e = self.calc_error()     # calc error [[dx1, dy1],  [dx2, dy2],  [dx3, dy3]]
+            _e_flat = _e.flatten()                           # flaten error [dx1, dy1, dx2, dy2, dx3, dy3]
+
+            _vel = - lambda_c * np.dot(_L_inv, _e_flat) # calculate velocity command vector
 
         # pack the Twist message
         velocity_cmd.linear.x = _vel[0]
