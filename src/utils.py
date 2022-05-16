f466bead37d77467d34c2e1e3d83776f240cc111
Armin Wessel <armin.wessel95@gmail.com>
2022-05-16 19:52:20 +0200

    Add pose estimation Use Pose estimation to measure Z Move the definition of the targets to launchfile, also fix target depth to be approximately in accordance with the physical projection Update the method update_interaction matrix to be able to use all 3 approximations mentioned by Chaumette

--- src/utils.py
+++ src/utils.py
@@ -40,3 +40,32 @@ def tf_cc(coordinates : np.array) -> np.array:
         return np.zeros((0,2))
     width, height = 640, 480 # x=0..640, y=0..480
     return np.subtract(np.repeat([[320,240]],4, axis=0), coordinates)
+
+def conv_pix_2_norm(coordinates: np.array, camera_matrix: np.array) -> np.array:
+    """
+    Use the camera parameters to calculate coordinates of the normalized plane (x,y) from pixel coordinates (u,v)
+    """
+    _c_u = camera_matrix[0,2]
+    _c_v = camera_matrix[1,2]
+    _f_x = camera_matrix[0,0]
+    _f_y = camera_matrix[1,1]
+    if not np.shape(coordinates) == (4,2):
+        rospy.ERROR("Array passed to conv_pix_2_norm does not have shape (4,2)")
+
+    coordinates_new = np.subtract(coordinates, np.repeat([[_c_u, _c_v]],4, axis=0))
+    coordinates_new = np.divide(coordinates, np.repeat([[_f_x, _f_y]],4, axis=0))
+    return coordinates_new
+
+def conv_norm_2_pix(coordinates: np.array, camera_matrix: np.array) -> np.array:
+    """
+    Use the camera parameters to calculate pixel coordinates (u,v) from the the normalized coordinates (x,y)
+    """
+    _c_u = camera_matrix[0,2]
+    _c_v = camera_matrix[1,2]
+    _f_x = camera_matrix[0,0]
+    _f_y = camera_matrix[1,1]
+    coordinates_new = np.multiply(coordinates, np.repeat([[_f_x, _f_y]],4, axis=0))
+    coordinates_new = np.add(coordinates, np.repeat([[_c_u, _c_v]],4, axis=0))
+    coordinates_new = coordinates_new.astype(int) # pixel values are always integers
+    return coordinates_new
+    
